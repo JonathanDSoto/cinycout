00145a96d0e53ef9f4da63093d056ed7e6deb9a6
Jonathan Soto <>
2021-02-08 11:28:00 -0700

    add some commetns

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
+//DIRECTIVAS DEL PROCESADOR
 #include <iostream>
 using namespace std;
+//DIRECTIVAS DEL PROCESADOR
 
 //aquí inicia la función principal
 int main() {
 
+    //declaración de variables
     string nombre;
+
     /*
      * Esto es
      * un bloque
      * de comentarios
      */
 
+    //el cout sirve para mostrar mensajes en consola
     cout << "Hola buenos días IDS TM 2020-I v2" << endl;
     cout << "Soy otro mensaje "  << endl  ;
     cout << "Soy el último mensaje \n" ;
 
+    //el cin para cpaturar los datos del teclado
     cin >> nombre;
 
     cout << "Hola " << nombre << " Bienvenido" << endl;
 
+    //el return indica que el programa ha finalizado
     return 0;
 }
