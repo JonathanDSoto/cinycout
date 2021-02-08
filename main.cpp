ab37102c6319dd8479667a2a31789a30f7958f1b
Jonathan Soto <>
2021-02-08 11:25:22 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//aquí inicia la función principal
+int main() {
+
+    string nombre;
+    /*
+     * Esto es
+     * un bloque
+     * de comentarios
+     */
+
+    cout << "Hola buenos días IDS TM 2020-I v2" << endl;
+    cout << "Soy otro mensaje "  << endl  ;
+    cout << "Soy el último mensaje \n" ;
+
+    cin >> nombre;
+
+    cout << "Hola " << nombre << " Bienvenido" << endl;
+
+    return 0;
+}
